0a8aaa27ec43140d13430ee8db2f5bbe1f8a605c
Lazizbek <oddiybolakay05@gmail.com>
2024-08-12 00:44:48 +0500

    api

--- src/pages/create-category/create-category.jsx
+++ src/pages/create-category/create-category.jsx
@@ -5,17 +5,21 @@ import { useNavigate } from "react-router-dom";
 import { useCategoryList } from "../../service/query/useCategoryList";
 import { toast } from "react-toastify";
 import { Loading } from "../../components/loading";
+import { useQueryClient } from "@tanstack/react-query";
 
 export const CreateCategory = () => {
   const { mutate } = useCreateCategory();
   const navigate = useNavigate();
   const { data, isLoading } = useCategoryList();
+  const client = useQueryClient()
 
   const submit = (data) => {
     mutate(data, {
       onSuccess: () => {
-        navigate("/");
+        navigate("/category-list");
         toast.success("category muvaffaqiyatli qo'shildi!");
+
+        client.invalidateQueries("category-list")
       },
     });
   };
